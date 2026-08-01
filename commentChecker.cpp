8a6d3f7d9cc3ab812e8686c7333461e543eca4d7
AelynnXu <102938935+AelynnXu@users.noreply.github.com>
2026-08-01 01:36:57 -0700

    added support for comments above function declaration
    also adjusted comment formatting and whitespace

--- commentChecker.cpp
+++ commentChecker.cpp
@@ -8,8 +8,9 @@
 int commentChecker(std::ifstream& inputFile){
     
     int warningCounter = 0;
-
-    if (!inputFile.is_open()) {//check if inputFile is reading
+    
+    //check if inputFile is reading
+    if (!inputFile.is_open()) {
         
         std::cout << "Error: commentChecker could not open file " << std::endl;
         return 0;
@@ -17,8 +18,9 @@ int commentChecker(std::ifstream& inputFile){
 
     std::vector<std::string> lines;//will contain input file as a vector of string lines
     std::string line;//will hold each line as it gets added to fileContent
-
-    while (std::getline(inputFile, line)){//copy inputFile's contents into lines vector one line at a time
+    
+    //copy inputFile's contents into lines vector one line at a time
+    while (std::getline(inputFile, line)){
         
         lines.push_back(line);
     }
@@ -26,39 +28,58 @@ int commentChecker(std::ifstream& inputFile){
     std::regex functionRegex(R"(^[a-zA-Z_][a-zA-Z0-9_<>\s\*\&]*\s+[a-zA-Z_][a-zA-Z0-9_]*\s*\([^\)]*\))");//regex to find functions
     std::regex commentRegex(R"((\/\/|\/\*|\*))");//regex to find comments
 
+    
+    //iterate through lines vector, searches for function within each line
+    for (int i = 0; i < lines.size(); ++i) {
 
-    for (int i = 0; i < lines.size(); ++i) {//iterate through lines vector
-
-        if (std::regex_search(lines[i], functionRegex)) {//searches for function within the line, assumes no comment until proven otherwise
+        
+        if (std::regex_search(lines[i], functionRegex)) {//detects function, assumes no comment until proven otherwise
             
             int functionLine = i + 1;//keeps track of function line for printing warning, +1 because i starts at 0
             bool hasComment = false;//flag for if there is a comment
+
+
+            //if i is not the first line of the file, checks if there is a preceding comment
+            if (i > 0) {
+                for (int j = i - 1; j >= 0; --j) {
+                    if (lines[j].find_first_not_of(" \t\r\n") == std::string::npos) {//skip empty lines/lines with only whitespace
+                        continue; 
+                    }
+                    if (std::regex_search(lines[j], commentRegex)) {//if the first non-empty line is a comment, set flag is true
+                        hasComment = true;
+                    }
+                    break; //break to stop from going back past the first non empty line
+                }
+            }
             
+
             if (std::regex_search(lines[i], commentRegex)) {//checks for inline comment
                 
                 hasComment = true;
             }
 
-            //reset these two
+            
+            //search function body line by line for comments, using brace height to detect start and end
             int insideFunction = false;//flag for use when scanning function interior
-            int braceDepth = 0;//for counting unclosed braces, for finding when the function ends
+            int braceDepth = 0;//for counting unclosed braces, used to find when the function ends
             int k = i;//iterator for inside function
-            while(k < lines.size()){//search function body for comments, using brace height to detect start and end
-                
-                for(char ch : lines[k]){//for loop going through current line to find braces
+            while(k < lines.size()){
+
+                //for loop goes through current line to find braces
+                for(char ch : lines[k]){
                     
                     if (ch == '{'){
                         
                         braceDepth++;
                         insideFunction = true;//after first brace means inside function body
                     }
-                    else if(ch == '}'){//closing brace means one less brace depth
+                    else if(ch == '}'){//closing one of the open braces means one less brace depth
                         
                         braceDepth--;
                     }
                 }
 
-                if(std::regex_search(lines[k], commentRegex)) {//check if this line has a comment
+                if(std::regex_search(lines[k], commentRegex)) {//check if current line has a comment
                     
                     hasComment = true;
                 }
@@ -82,5 +103,6 @@ int commentChecker(std::ifstream& inputFile){
         }
 
     }
+
     return warningCounter;
 }
