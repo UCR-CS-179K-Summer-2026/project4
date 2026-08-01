cefa01f09eaf04d8ac384540a0734bf449404864
j1munoz <jimmymunoz397@gmail.com>
2026-08-01 03:35:59 -0700

    Added SmellyDetector class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "redundantCodeChecker.h"
-#include "poorNameChecker.h"
-#include "Parser.h"
+#include "SmellyCodeDetector.h"
 
 int main() {
     int smellyCount = 0;//counter for any warnings found
@@ -20,16 +18,8 @@ int main() {
         return 1;
     }
 
-    Parser parser;
-    ParsedSource parsedSource = parser.parse(inputFile);
-
-    // All detector functions called here and added to smellyCount
-    RedundantCodeChecker redundantCodeChecker;
-    PoorNameChecker poorNameChecker;
-
-    //smellyCount = commentChecker(); //comment detector
-    smellyCount += redundantCodeChecker.analyzeSource(parsedSource); //redundant code detector
-    smellyCount += poorNameChecker.analyzeSource(parsedSource); //poor name detector
+    // // All detector functions called here and added to smellyCount
+    smellyCount += SmellyCodeDetector(inputFile).runDetectors(); //run all detectors
     
     if (smellyCount > 0){ //smelly code found
         std::cout << smellyCount << " Smelly code found in the file." << std::endl;
@@ -40,9 +30,7 @@ int main() {
         std::cout << "No smelly code found in the file." << std::endl;
     }
 
-
     inputFile.close();
-
 
     return 0;
 }
