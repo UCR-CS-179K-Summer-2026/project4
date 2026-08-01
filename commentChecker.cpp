53e53a0aafa047b4fd5c96fe34589f87a81b7dbd
AelynnXu <102938935+AelynnXu@users.noreply.github.com>
2026-08-01 01:11:26 -0700

    pretesting version 2
    fixed infinite loop, probably

--- commentChecker.cpp
+++ commentChecker.cpp
@@ -26,25 +26,24 @@ int commentChecker(std::ifstream& inputFile){
     std::regex functionRegex(R"(^[a-zA-Z_][a-zA-Z0-9_<>\s\*\&]*\s+[a-zA-Z_][a-zA-Z0-9_]*\s*\([^\)]*\))");//regex to find functions
     std::regex commentRegex(R"((\/\/|\/\*|\*))");//regex to find comments
 
-    bool hasComment = false;//flag for if there is a comment
-    int braceDepth = 0;//for counting unclosed braces, for finding when the function ends
-    int insideFunction = false;//flag for use when scanning function interior
-    int functionLine = 0;//keeps track of function line for printing warning
 
     for (int i = 0; i < lines.size(); ++i) {//iterate through lines vector
+
         if (std::regex_search(lines[i], functionRegex)) {//searches for function within the line, assumes no comment until proven otherwise
-            functionLine = i;
-            hasComment = false;
+            
+            int functionLine = i + 1;//keeps track of function line for printing warning, +1 because i starts at 0
+            bool hasComment = false;//flag for if there is a comment
             
             if (std::regex_search(lines[i], commentRegex)) {//checks for inline comment
                 
                 hasComment = true;
             }
 
-            insideFunction = false;//reset these two for the loop
-            braceDepth = 0;
-
-            for(int k = i; k < lines.size(); ++k){//search function body for comments, using brace height to detect start and end
+            //reset these two
+            int insideFunction = false;//flag for use when scanning function interior
+            int braceDepth = 0;//for counting unclosed braces, for finding when the function ends
+            int k = i;//iterator for inside function
+            while(k < lines.size()){//search function body for comments, using brace height to detect start and end
                 
                 for(char ch : lines[k]){//for loop going through current line to find braces
                     
@@ -64,17 +63,17 @@ int commentChecker(std::ifstream& inputFile){
                     hasComment = true;
                 }
 
-                if (insideFunction && k < lines.size()) {//if we entered a function, and didn't reach the end of the file yet, set i = k so we don't rescan the lines between
-                
-                    i = k; 
-                }
-
                 if(insideFunction && braceDepth <= 0){//if braceDepth reaches 0 after having entered the function, then the function has ended
                     
                     break;
                 }
-            }
 
+                k++;//increment k to the next line
+            }
+            if (insideFunction && k < lines.size()) {//if we entered a function, and didn't reach the end of the file yet, set i = k so we don't rescan the lines between
+                
+                    i = k; 
+            }
             if(!hasComment){//if no comments detected, add counter and print warning
 
                 std::cout << "Warning: function in line " << functionLine << " has no comments" << std::endl;
