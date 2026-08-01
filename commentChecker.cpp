5d5fdb72b54382b1d53ea8b486c36d5136bb0aa7
AelynnXu <102938935+AelynnXu@users.noreply.github.com>
2026-07-31 23:41:33 -0700

    changed file reading method
    added back commentsRegex and functionRegex, now converts input into a string instead of trying to run the regex line by line. Detection not yet reimplemented

--- commentChecker.cpp
+++ commentChecker.cpp
@@ -4,6 +4,20 @@
 #include <string>
 
 
-int commentChecker(ifstream inputFile){
+int commentChecker(ifstream& inputFile){
+    if (!inputFile.is_open()) {//check if inputFile is reading
+        std::cout << "Error: commentChecker could not open file " << std::endl;
+        return 0;
+    }
+
+    std::string fileContent;//will contain input file as a string
+    std::string line;//will hold each line as it gets added to fileContent
+
+    while (std::getline(inputFile, line)){//copy inputFile's contents into fileContent line by line
+        fileContent += line + "\n";
+    }
+
+    std::regex commentsRegex(R"(//.*?$|/\*.*?\*/)");//translation of comments into regex
+    std::regex functionRegex(R"((?:^|\s)[a-zA-Z_][a-zA-Z0-9_<>\s::]*\s+[a-zA-Z_][a-zA-Z0-9_]*\s*\([^)]*\)[^{;]*\{)");//translation of functions into regex
 
 }
