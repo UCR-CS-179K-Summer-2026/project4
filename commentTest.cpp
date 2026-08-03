63e5218ca6099809d321de536e279a64facd40c1
AelynnXu <102938935+AelynnXu@users.noreply.github.com>
2026-08-03 14:50:04 -0700

    parser modifications
    added stripStrings function to parser, made parser functions public for accessibility from detectors, and addressed string literal brackets edge case for commentChecker.
    
    Note: commentTest should now print its warning on line 4.

--- commentTest.cpp
+++ commentTest.cpp
@@ -1,3 +1,6 @@
+#include <string>
+
+
 int uncommentedFunction(int num) {
     return num;
 }
@@ -20,3 +23,9 @@ int commentedInFunction(int num){
 int commentEndOfFunction(int num){
     return num;
 }//comment on end bracket line
+
+
+int commentedBeforeFunction(int num){
+    std::string testerString = "}}}";
+    return num;//previous line has closing brackets so if strings are not stripped the checker will not see this comment
+}
