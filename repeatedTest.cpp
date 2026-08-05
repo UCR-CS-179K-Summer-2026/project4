199ab8e47c53c200c461988fe9da70d6b6e1c319
Stuart Arief <stuartarief@ucr-secure-21-10-13-133-115.wnet.ucr.edu>
2026-08-05 14:54:58 -0700

    Revamped repeatedTest.cpp test functions

--- repeatedTest.cpp
+++ repeatedTest.cpp
@@ -1,4 +1,6 @@
-int computeStats(int a, int b) {
+#include <iostream>
+
+int normalRepeatBlocks(int a, int b) {
     int result = 0;
     result = a + b;
     result = a - b;
@@ -6,5 +8,70 @@ int computeStats(int a, int b) {
     result = a + b;
     result = a - b;
     result = a * b;
+
+    std::string message = " ";
+
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+
     return result;
+}
+
+
+void shouldBeLoop(){
+    int numbers[5] = {}, index = 0;
+
+    numbers[index] += 1;
+    index++;
+
+    numbers[index] += 1;
+    index++;
+
+    numbers[index] += 1;
+    index++;
+
+    numbers[index] += 1;
+    index++;
+
+    numbers[index] += 1;
+    index++;
+
+    numbers[index] += 1;
+    index++;
+
+}
+
+void repeatedButDiffOrder(int a, int b){
+    int result = 0;
+    result = a + b;
+    result = a - b;
+    result = a * b;
+
+    result = a * b;
+    result = a - b;
+    result = a + b;
+
+}
+
+void repeatedButOverlapped(){
+
+    std::string message = " ";
+
+    message += "Hello";
+    message += "Hello";
+    std::cout << "Hello" << std::endl;
+    message += "Hello";
+    message += "Hello";
+
+    int foo = 1;
+    message += "Hello";
+    message += "Hello";
+    message += "Hello";
+
+
 }
