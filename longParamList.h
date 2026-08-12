e0cff1236fb7860faa5614fb752cb342d0e290e9
j1munoz <jimmymunoz397@gmail.com>
2026-08-12 16:35:46 -0700

    Fixed issue when merging

--- longParamList.h
+++ longParamList.h
@@ -9,16 +9,16 @@ class longParamList: public Detector{
 
 public:
     longParamList() = default;
-    int analyzeSource(const ParsedSource& source) override;
+    std::vector<Warning> analyzeSource(const ParsedSource& source) override;
 
 private:
-    void visitNode(TSNode node, const ParsedSource& parsedSource, int& warningCount) override;
+    virtual void visitNode(TSNode node, const ParsedSource& parsedSource, std::vector<Warning>& warnings) override;
 
     static constexpr int kMaxParams = 4;
 
     TSNode findIDNode(TSNode node) const;
     std::string extractFunctionName(TSNode functionDefNode, const std::string& source) const;
 
-    void reportLongParamList(const std::string& functionName, int paramCount, int line ) const;
+    void reportLongParamList(const std::string& functionName, int paramCount, int line, std::vector<Warning>& warnings) const;
 };
 #endif 
