34699df4dad83f2d5aee2c01d469d79c084b14b9
AelynnXu <102938935+AelynnXu@users.noreply.github.com>
2026-08-25 22:23:34 -0700

    Update memoryChecker.h

--- memoryChecker/memoryChecker.h
+++ memoryChecker/memoryChecker.h
@@ -19,7 +19,7 @@ class memoryChecker : public Detector {
         bool checkIfBodyDeallocates(TSNode node, const std::string& paramName, const std::string& src);
         void collectDeallocatingFunctions(TSNode node, const ParsedSource& parsedSource);
         std::string getAllocationVariable(TSNode node, const std::string& src);//gets variable that corresponds to an allocation
-        void analyzeFunction(TSNode functionNode, const ParsedSource& parsedSource, std::vector<Warning>& warnings);
+        //void analyzeFunction(TSNode functionNode, const ParsedSource& parsedSource, std::vector<Warning>& warnings);
         
         std::unordered_map<std::string, int> trackedAllocations;//variable names and line numbers for each memory allocation detected
         std::unordered_map<std::string, std::set<int>> deallocatingFunctions;//list of deallocating functions
